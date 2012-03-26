df509cb579673315860df6d060a984a59aab73ff
Elisami <ecf2607_@hotmail.com>
2012-03-26 02:18:48 -0600

    Entidad de Asistentes

---
+++ ProyectoGP/src/entidades/Asistente.java
@@ -0,0 +1,12 @@
+package modelos;
+
+public class Asistente extends Usuario {
+
+		public Asistente(String idUsuario, String nombre, String apellido,
+			String nombreUsuario, String clave, String rol, String cedula,
+			String telefonos, String direccion) {
+		super(idUsuario, nombre, apellido, nombreUsuario, clave, rol, cedula,
+				telefonos, direccion);
+	}		
+}
+
