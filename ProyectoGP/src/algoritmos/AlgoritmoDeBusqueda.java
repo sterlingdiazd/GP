3add5d67d3792671225a5d22aedd07112f4e6b58
Elisami <ecf2607_@hotmail.com>
2012-03-26 03:56:55 -0600

    Algoritmo para buscar pacientes

---
+++ ProyectoGP/src/algoritmos/AlgoritmoDeBusqueda.java
@@ -0,0 +1,8 @@
+package algoritmos;
+
+import java.util.ArrayList;
+
+public interface AlgoritmoDeBusqueda {
+   public ArrayList<Paciente> buscarPaciente(ArrayList<Paciente> pacientes, String preferencias);
+   
+}
