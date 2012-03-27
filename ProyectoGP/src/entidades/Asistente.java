0d8b43218ef9325d10adda714186c3af2883a960
Elisami <ecf2607_@hotmail.com>
2012-03-27 02:22:53 -0600

    modificacion

--- ProyectoGP/src/entidades/Asistente.java
+++ ProyectoGP/src/entidades/Asistente.java
@@ -1,12 +1,33 @@
-package modelos;
+package entidades;
 
 public class Asistente extends Usuario {
+	
+	public String idAsistente;
+	public String nombreUsuario;
+	
 
-		public Asistente(String idUsuario, String nombre, String apellido,
-			String nombreUsuario, String clave, String rol, String cedula,
-			String telefonos, String direccion) {
-		super(idUsuario, nombre, apellido, nombreUsuario, clave, rol, cedula,
-				telefonos, direccion);
-	}		
+	public Asistente( String idUsuario, String nombreUsuario, String clave, String rol, String idAsistente, 
+			        	String nombre, String apellido, String telefonos, String direccion, String cedula)
+	{
+		super(idUsuario, nombre, apellido, nombreUsuario,  clave,  rol,  cedula, telefonos,  direccion);
+		this.idAsistente = idAsistente;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+		public String getIdAsistente() {
+			return idAsistente;
+		}
+
+		public void setIdAsistente(String idAsistente) {
+			this.idAsistente = idAsistente;
+		}	
+		
+		public String getNombreUsuario() {
+			return nombreUsuario;
+		}
+
+		public void setNombreUsuario(String nombreUsuario) {
+			this.nombreUsuario = nombreUsuario;
+		}
+
 }
-
